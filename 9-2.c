f58accb29c36d6e9119d07c685b7c52c50d47db9
tls0000 <127270565+tls0000@users.noreply.github.com>
2024-05-10 08:54:58 +0900

    Add files via upload

---
+++ 9-2.c
@@ -0,0 +1,18 @@
+3include <stdio.h>
+void main()
+	char stack[5];
+	int top=0;
+	char carName =
+	int select=9:
+	vihile (select != 3)
+	pr intf("<1> 자동차 넣기 <2> 자동차 빼기 <3> 끝 :
+			scanf("%d", &select)
+			switch(select)
+			case
+			( top >= printf("터널이 pron:7(.3이 5 팍 차서 차가 못 들어감\n")
+			  else
+			  stack[ top] B carName++
+			  f
+			  pr inti(" %c 자동차가 터널에 들어김wn, stack[
+				  top t
+				  top]);
