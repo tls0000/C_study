458366ac73c2f167259b2083325edf71a1a03237
tls0000 <127270565+tls0000@users.noreply.github.com>
2024-06-11 14:52:20 +0900

    Add files via upload

---
+++ 12-3.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <malloc.h>
+
+void main()
+{
+	int* p;
+	int i, hap=0; 
+	int cnt;
+
+	printf(" ÀÔ·ÂÇÒ °³¼ö´Â?");
+       	scanf("%p),
+		
+}d", &cnt);
+	*
+		* p = (int)malloc(sizeof(int) cnt),
+		* for(i=0; i<cnt; i++)
+			* for(i=0
+					* {
+					* }
+					* ¿¥ºê
+					* printf("%d | I¥ð ", i+1); scanf("%d", p+i);
+					* for(i=0; i<cnt; i++)
+					* hap hap + (p+i);
+					* FO<<
+						* printf("ÎáÎ
